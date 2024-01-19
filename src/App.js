4b7117f7e7cad4d73ce048cb751bcc3e625495a1
C1oudys <hymin310@gmail.com>
2024-01-19 15:37:55 +0900

    제목내용 추가 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
+import React, { useState } from "react";
 import "./App.css";
-import "./components/Header";
-function App() {
+
+const App = () => {
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const titleSaveHandler = (event) => {
+    setTitle(event.target.value);
+  };
+  const textSaveHandler = (event) => {
+    setText(event.target.value);
+  };
+
+  const addButtonClickHandler = () => {
+    const newTodo = { id: todos.length + 1, title, text, isDone: false };
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
+    setTitle("");
+    setText("");
+  };
+
+  const deleteBtnClickHandler = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
   return (
     <div>
-      <Header />
+      <h1>KIM's Todo List</h1>
+      <div>
+        제목
+        <input value={title} onChange={(event) => titleSaveHandler(event)} />
+        내용
+        <input value={text} onChange={(event) => textSaveHandler(event)} />
+        <button className="addBtn" onClick={addButtonClickHandler}>
+          추가하기
+        </button>
+      </div>
+      <div>
+        {todos.map((todo) => (
+          <div key={todo.id}>
+            {todo.title}
+            <br />
+            {todo.text}
+            <button onClick={() => deleteBtnClickHandler(todo.id)}>삭제</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
